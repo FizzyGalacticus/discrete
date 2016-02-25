d68fc3ad7675dac41f6d3bc1435710dad6567e1a
Dustin L. Dodson <dustinleedodson@gmail.com>
2016-02-24 16:31:13 -0900

    Added programming problems for chapter 3.

--- homework/ch3.cpp
+++ homework/ch3.cpp
@@ -1,6 +1,30 @@
 /*
 Chapter 3 Homework
 Computer Exercises
+1. Implement a hashing system for storing integers in an array.
+2. Write a program that determines whether an ISBN is valid.
+3. Write a program that generates pseudorandom integers.
+
+In Exercises 4-9, assume that a sequence from {1,...,n} to the real 
+numbers is represented as an array A, indexed from 1 to n.
+
+4. Write a program that tests whether A is one to one.
+5. Write a program that tests whether A is onto a given set.
+6. Write a program that tests whether A is increasing.
+7. Write a program that tests whether A is decreasing.
+8. Write a program that tests whether A is nonincreasing.
+9. Write a program that tests whether A is nondecreasing.
+
+10. Write a program to determine whether one sequence is a subsequence of another sequence.
+11. Write a program to determine whether one string is a substring of another string.
+12. Write a program to determine whether a relation is reflexive.
+13. Write a program to determine whether a relation is antisymmetric.
+14. Write a program to determine whether a relation is transitive.
+15. Write a program that finds the inverse of a relation.
+16. Write a program that finds the composition (R of S) of relations R and S.
+17. Write a proram that checks whether a relation R is an equivalence relation. If R is an 
+equivalence relation, the program outputs the equivalence classes of R.
+18. Write a program to determine whether a relation is a function from a set X to a set Y.
 */
 
 int main(int argc, char const *argv[])
